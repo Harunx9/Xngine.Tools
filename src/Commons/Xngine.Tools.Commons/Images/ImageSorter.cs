a13b4c16e7f48973c5095e3a172dd0c61204f989
Szymon Wanot <szwanot@gmail.com>
2017-08-12 20:26:49 +0200

    ADD: Image sorter

---
+++ src/Commons/Xngine.Tools.Commons/Images/ImageSorter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Xngine.Tools.Commons.Images
+{
+    public enum SortDir
+    {
+        ASC,
+        DESC
+    }
+
+    public interface IImageSorter
+    {
+        IEnumerable<IImage> SortBy<TKey>(Func<IImage, TKey> sortProperty, IEnumerable<IImage> collectionToSort, SortDir dir);
+    }
+
+    public class ImageSorter : IImageSorter
+    {
+        public IEnumerable<IImage> SortBy<TKey>(Func<IImage, TKey> sortProperty, IEnumerable<IImage> collectionToSort, SortDir dir)
+        {
+            switch (dir)
+            {
+                case SortDir.ASC:
+                    return collectionToSort.OrderBy(sortProperty).ToArray();
+                case SortDir.DESC:
+                    return collectionToSort.OrderByDescending(sortProperty).ToArray();
+            }
+            return collectionToSort.ToArray();
+        }
+    }
+}
