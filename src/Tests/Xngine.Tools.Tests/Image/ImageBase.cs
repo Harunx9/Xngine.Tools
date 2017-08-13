6b7048d4fab81f9f902e9c074cc2a86e0e3f44c9
Szymon Wanot <szwanot@gmail.com>
2017-08-13 13:44:34 +0200

    ADD: test for images and image sorting

---
+++ src/Tests/Xngine.Tools.Tests/Image/ImageBase.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using Xngine.Tools.Tests.BaseFramework;
+
+namespace Xngine.Tools.Tests.Image
+{
+    public abstract class ImageBase : AAATest
+    {
+        protected string TilesImagePath => Path.Combine(Directory.GetCurrentDirectory(), "TestData/Images/tiles.png");
+        protected string MeticsImagePath => Path.Combine(Directory.GetCurrentDirectory(), "TestData/Images/metrics.png");
+    }
+}
---
+++ src/Tests/Xngine.Tools.Tests/Image/ImageRgba32Test.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using Xngine.Tools.Commons.Images;
+using Xngine.Tools.Tests.BaseFramework;
+
+namespace Xngine.Tools.Tests.Image
+{
+    public class on_image32_load_from_file : ImageBase
+    {
+        private ImageRgba32 image;
+
+        protected override void Arrange(){}
+
+        protected override void Act()
+        {
+            image = new ImageRgba32(TilesImagePath);
+        }
+
+        [Assert]
+        public void loaded_image_should_not_be_null() 
+            => image.Should().NotBeNull();
+
+        [Assert]
+        public void loaded_image_should_not_be_empty()
+            => image.IsEmpty.Should().BeFalse();
+
+        [Assert]
+        public void loaded_image_should_have_pixels_greater_than_0()
+            => image.PixelCount.Should().BeGreaterThan(0);
+    }
+
+    public class on_create_empty_image32 : ImageBase
+    {
+        private ImageRgba32 image;
+
+        protected override void Arrange() { }
+
+        protected override void Act()
+        {
+            image = new ImageRgba32(60, 60);
+        }
+
+        [Assert]
+        public void created_image_should_not_be_null()
+            => image.Should().NotBeNull();
+
+        [Assert]
+        public void created_image_should_be_empty()
+            => image.IsEmpty.Should().BeFalse();
+
+        [Assert]
+        public void loaded_image_should_have_pixels_greater_than_0()
+            => image.PixelCount.Should().BeGreaterThan(0);
+    }
+}
