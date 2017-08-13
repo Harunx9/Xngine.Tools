fde3bb28b274ea850276a027679fb2d342bd18a6
Szymon Wanot <szwanot@gmail.com>
2017-08-13 13:45:33 +0200

    EDIT: Suppress test nameing conventions errors

--- src/Tests/Xngine.Tools.Tests/GlobalSuppressions.cs
+++ src/Tests/Xngine.Tools.Tests/GlobalSuppressions.cs
@@ -4,6 +4,11 @@
 // Project-level suppressions either have no target or are given 
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Xngine.Tools.Tests.Xml.XmlSerializerTests.xml_class_can_be_serialized_to_string")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Xngine.Tools.Tests.Xml.SpritesheetTest.serialized_spritesheet_should_have_valid_schema")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "type", Target = "~T:Xngine.Tools.Tests.Xml.on_spitesheet_serialize")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "type", Target = "~T:Xngine.Tools.Tests.Xml.on_test_serialize")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "type", Target = "~T:Xngine.Tools.Tests.Xml.on_test_deserialize")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "type", Target = "~T:Xngine.Tools.Tests.Xml.on_spitesheet_deserialize")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "type", Target = "~T:Xngine.Tools.Tests.Xml.on_animation_spritesheet_serialization")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "type", Target = "~T:Xngine.Tools.Tests.Xml.on_animation_spritesheet_deserialization")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "type", Target = "~T:Xngine.Tools.Tests.Image.on_asc_image_collection_sort")]
 
